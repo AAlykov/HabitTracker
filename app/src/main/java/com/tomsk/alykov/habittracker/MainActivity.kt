c0010d46a6f784c913b62060ba56b45d5e88f89e
Alykov <aalykov@gmail.com>
2022-11-20 13:17:29 +0700

    Fix error about transfer Fragments into UI

--- app/src/main/java/com/tomsk/alykov/habittracker/MainActivity.kt
+++ app/src/main/java/com/tomsk/alykov/habittracker/MainActivity.kt
@@ -9,3 +9,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+
+//setupActionBarWithNavController(findNavController(R.id.navHostfragment))
+
+
+/*override fun onSupportNavigateUp(): Boolean {
+    val navController = findNavController(R.id.navHostfragment)
+    return navController.navigateUp() || super.onSupportNavigateUp()
+}*/
